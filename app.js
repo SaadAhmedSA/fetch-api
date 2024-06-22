cec2d6624b68a06f1a76fcdc5cef94964c8b1154
saad ahmed <ss5099903@gmail.com>
2024-06-22 13:35:10 +0500

    ok

---
+++ app.js
@@ -0,0 +1,18 @@
+let url = "https://cat-fact.herokuapp.com/facts"
+
+  async function old(){
+
+let respon = await fetch(url)
+
+
+let dat = await respon.json()
+
+
+console.log(dat[0].text);
+console.log(dat[1].text);
+console.log(dat[2].text);
+console.log(dat[3].text);
+console.log(dat[4].text);
+
+ }
+old()
